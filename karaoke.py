1ed7b8e816f434cbc5adad95949537a0d7c2a289
Hugo Bravo Diaz <hbravod@alpha18.aulas.gsyc.urjc.es>
2017-10-16 14:43:53 +0200

    pep8 check

--- karaoke.py
+++ karaoke.py
@@ -9,41 +9,47 @@ from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
 
-class KaraokeLocal():
+class KaraokeLocal(SmallSMILHandler):
 
-    def Inicializador(SmallSMILHandler):
-        lista = cHandler.get_tags()
+    def __init__(self):
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        self.list = cHandler.get_tags()
+        try:
+            parser.parse(open(sys.argv[1]))
+        except SyntaxError:
+            print("Usage: python3 karaoke.py file.smil")
 
     def __str__(self):
-        for labelsD in lista:
-            print(labelsD['name'], "\t",)
-        for atributo in labelsD:
-            if labelsD[atributo] != "" and atributo != 'name':
-                print(atributo, "=", '"', labelsD[atributo], '"')
+        list_str = ''
+        for labelsD in self.list:
+            list_str = list_str + labelsD['name']
+            for atributo in labelsD:
+                if labelsD[atributo] != "" and atributo != 'name':
+                    list_str = list_str + "\t" + atributo + '='
+                    list_str = list_str + labelsD[atributo] + '"' + "\n"
+        return(list_str)
 
-    def to_json(self):
-        with open('karaoke.json', 'w') as outfile:
-            json.dump(lista, outfile)
+    def to_json(self, ficheroS, ficheroJ=''):
+        if ficheroJ == '':
+            ficheroJ = ficheroS.split('.')[0] + '.json'
+        with open(ficheroJ, 'w') as outfile:
+            json.dump(self.list, outfile)
 
     def do_local(self):
-        for labelsD in lista:
-            print(labelsD['name'], "\t",)
-        for atributo in labelsD:
-            if labelsD[atributo][0:7] == "http://":
-                atributolargo = labelsD[atributo]
-                atributocorto = labelsD[atributo].split('/')[-1]
-                urllib.request.urlretrieve(atributolargo, atributocorto)
-                labelsD[atributo] = atributocorto
-            if labelsD[atributo] != "" and atributo != 'name':
-                print(atributo, "=", '"', labelsD[atributo], '"')
+        for labelsD in self.list:
+            for atributo in labelsD:
+                if labelsD[atributo][0:7] == "http://":
+                    atributolargo = labelsD[atributo]
+                    atributocorto = labelsD[atributo].split('/')[-1]
+                    urllib.request.urlretrieve(atributolargo, atributocorto)
+                    labelsD[atributo] = atributocorto
 
 if __name__ == "__main__":
-
-    parser = make_parser()
-    cHandler = KaraokeLocal()
-    parser.setContentHandler(cHandler)
-
-    try:
-        parser.parse(open(sys.argv[1]))
-    except SyntaxError:
-        print("Usage: python3 karaoke.py file.smil")
+    objeto = KaraokeLocal()
+    print(objeto)
+    objeto.to_json(sys.argv[1])
+    objeto.do_local()
+    objeto.to_json(sys.argv[1], 'local.json')
+    print(objeto)
